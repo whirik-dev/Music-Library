e1e652414ff98c9fbc48ed53a73772a3fba42997
changhyunme <changhyun.me@gmail.com>
2025-09-12 11:11:45 +0900

    change signin ui

--- components/auth/SignInPage.jsx
+++ components/auth/SignInPage.jsx
@@ -16,6 +16,8 @@ const SignInPage = () => {
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
 
+    const [socialSignIn, setSocialSignIn] = useState(true);
+
 
     // Login with Credential
     const handleLogin = async () => {
@@ -52,36 +54,70 @@ const SignInPage = () => {
         <div className="flex flex-col">
             <div className="flex flex-row gap-10 p-10 relative">
                 {/* Sign Up Area */}
-                <div className="flex-1 flex flex-col gap-3">
+                {/* <div className="flex-1 flex flex-col gap-3">
                     <span className="text-3xl mb-10">{t('sign_up')}</span>
-                    {/* <span>회원가입 독려 메시지, ex)지금 바로 고퀄리티 라이브러리의 접근권한을 얻으세요! 등...</span> */}
-
                     <Button name={t('sign_up')} onClick={()=>setModalPage('signup')} className="mt-auto"/>
-                </div>
+                </div> */}
 
                 {/* Sign In Area */}
                 <div className="flex-1 flex flex-col gap-3">
-                    <span className="text-3xl mb-10">{t('sign_in')}</span>
-                    <SocialLoginButton method="google" onClick={() => signIn('google')}/>
-                    <SocialLoginButton method="x" />
-                    <Divider name={t('divider_or')} />
-                    <InputField
-                        placeholder={t('username_email_placeholder')}
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <InputField
-                        type="password"
-                        placeholder={t('password_placeholder')}
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <Button name={t('sign_in')} onClick={handleLogin} />
-                    {error && <p className="text-sm text-red-500">{error}</p>}
+                    <span className="text-3xl mb-10 text-center">{t('sign_in')}</span>
+
+                    {socialSignIn ? (
+                        <>
+                            <div className="flex flex-row justify-center">
+                                <div className="w-1/2 flex flex-col gap-3">
+                                    <SocialLoginButton method="google" onClick={() => signIn('google')}/>
+                                    {/* <SocialLoginButton method="x" /> */}
+
+                                    <Divider name={t('divider_or')} />
+                                    <div className="flex flex-col justiy-center">
+                                        <div className="text-foreground/20 hover:underline cursor-pointer hover:text-foreground/40 text-center"
+                                             onClick={() => setSocialSignIn(false)}
+                                        >
+                                            {t('change_signin_method_to_credentials')}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <div className="flex flex-row justify-center">
+                                <div className="w-1/2 flex flex-col gap-3">
+                                    <InputField
+                                        placeholder={t('username_email_placeholder')}
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                    />
+                                    <InputField
+                                        type="password"
+                                        placeholder={t('password_placeholder')}
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                    />
+                                    <Button name={t('sign_in')} onClick={handleLogin} />
+                                    {error && <p className="text-sm text-red-500">{error}</p>}
+
+                                    {/* <Divider name={t('divider_or')} /> */}
+                                    <div className="flex flex-row justify-between">
+                                        <div className="text-foreground/20 hover:underline cursor-pointer hover:text-foreground/40 text-center"
+                                             onClick={() => setSocialSignIn(true)}
+                                        >
+                                            {t('change_signin_method_to_social')}
+                                        </div>
+                                        <div className="text-foreground/20 hover:underline cursor-pointer hover:text-foreground/40 text-center">
+                                            sign up?
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    )}
                 </div>
 
                 {/* divider */}
-                <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[1px] h-full bg-zinc-300/10" />
+                {/* <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[1px] h-full bg-zinc-300/10" /> */}
             </div>
             <div className="flex text-xs border-t-1 border-zinc-800 p-3 text-zinc-600">
                 {t('terms_agreement')}
