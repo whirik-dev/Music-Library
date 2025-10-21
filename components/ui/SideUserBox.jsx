cd93d8de68bd43477aabda71d981ab47bb552d30
changhyunme <changhyun.me@gmail.com>
2025-10-21 14:06:27 +0900

    add org card

--- components/ui/SideUserBox.jsx
+++ components/ui/SideUserBox.jsx
@@ -10,6 +10,7 @@ import ProgressBar from "@/components/ui/ProgressBar"
 import useMusicItemStore from "@/stores/useMusicItemStore";
 
 import SignOutBtn from "@/components/auth/SignOutButton";
+import OrganizationBox from "@/components/ui/OrganizationBox";
 
 
 const SideUserBox = () => {
@@ -36,45 +37,49 @@ const SideUserBox = () => {
     };
 
     return (
-        <div className={`bg-foreground/5 p-3 rounded-sm select-none transition-normal duration-300 ${playingTrackId && "last:mb-16"}`}>
-            <div className="font-bold text-md text-nowrap overflow-x-hidden text-foreground">
-                {userInfo.name}<br/>
-                {/* <span className="text-sm font-medium opacity-50">{userInfo.email}</span> */}
-            </div>
-            <div className="font-normal text-foreground text-sm"
-            >
-                <span className="uppercase bg-foreground/10 py-0.5 px-1 rounded-md text-xs font-black cursor-pointer hover:opacity-80"
-                      onClick={handleCreditClick}
+        <div className="flex flex-col gap-2">
+            <OrganizationBox email={userInfo.email} />
+
+            <div className={`bg-foreground/5 p-3 rounded-sm select-none transition-normal duration-300 ${playingTrackId && "last:mb-16"}`}>
+                <div className="font-bold text-md text-nowrap overflow-x-hidden text-foreground">
+                    {userInfo.name}<br/>
+                    {/* <span className="text-sm font-medium opacity-50">{userInfo.email}</span> */}
+                </div>
+                <div className="font-normal text-foreground text-sm"
                 >
-                    {membership}
-                </span>
-                 
-                <span className="text-xs ml-2 cursor-pointer hover:opacity-80"
-                      onClick={handleCreditClick}
-                >
-                    {credits} Credits
-                </span>
-            </div>
-            <div className="mt-9">
-                <div className="flex flex-row mb-1 text-sm justify-between">
-                    <div className="text-foreground">Download</div>
-                    <div className="capitalize font-bold">
-                        {membership != 'free' ? "unlimited" : `${downloadPoints < 0 ? "0" : downloadPoints}/10`}
+                    <span className="uppercase bg-foreground/10 py-0.5 px-1 rounded-md text-xs font-black cursor-pointer hover:opacity-80"
+                        onClick={handleCreditClick}
+                    >
+                        {membership}
+                    </span>
+                    
+                    <span className="text-xs ml-2 cursor-pointer hover:opacity-80"
+                        onClick={handleCreditClick}
+                    >
+                        {credits} Credits
+                    </span>
+                </div>
+                <div className="mt-9">
+                    <div className="flex flex-row mb-1 text-sm justify-between">
+                        <div className="text-foreground">Download</div>
+                        <div className="capitalize font-bold">
+                            {membership != 'free' ? "unlimited" : `${downloadPoints < 0 ? "0" : downloadPoints}/10`}
+                        </div>
                     </div>
+                    <ProgressBar now={downloadPoints} max={downloadPoints>10 ? downloadPoints : "10"} plan={membership}/>
                 </div>
-                <ProgressBar now={downloadPoints} max={downloadPoints>10 ? downloadPoints : "10"} plan={membership}/>
-            </div>
-            {/* <Link href="/(.)preference"> */}
-            <div className="flex flex-row items-center justify-between mt-2">
-                <div className="flex flex-row items-center gap-1 cursor-pointer hover:opacity-50" 
-                     onClick={handlePreferenceClick}
-                >
-                    <IconSettings size="18"/>
-                    Preference
+                {/* <Link href="/(.)preference"> */}
+                <div className="flex flex-row items-center justify-between mt-2">
+                    <div className="flex flex-row items-center gap-1 cursor-pointer hover:opacity-50" 
+                        onClick={handlePreferenceClick}
+                    >
+                        <IconSettings size="18"/>
+                        Preference
+                    </div>
+                    <SignOutBtn />
                 </div>
-                <SignOutBtn />
+                {/* </Link> */}
             </div>
-            {/* </Link> */}
         </div>
     )
 }
